dab645d16b50080b6533c7b99ba6d1d9c5a18e01
Nicolas Vannieuwkerke <nicolas.vannieuwkerke@ugent.be>
2022-09-02 15:26:39 +0200

    linting

--- lib/WorkflowNfcmggstructural.groovy
+++ lib/WorkflowNfcmggstructural.groovy
@@ -8,7 +8,6 @@ class WorkflowNfcmggstructural {
     // Check and validate parameters
     //
     public static void initialise(params, log) {
-        
 
         if (!params.fasta) {
             log.error "Genome fasta file not specified with e.g. '--fasta genome.fa' or via a detectable config file."
